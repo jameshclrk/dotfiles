c52ffab277955fe814112bfcbe8ac3e2d4f6761d
James Clark <james@jamesclark.dev>
2022-02-02 18:36:13 +0000

    Pretty Telescope + preload plugins

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -2,6 +2,22 @@ local tele = require("telescope")
 local actions = require("telescope.actions")
 tele.setup {
   defaults = {
+    borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
+    layout_config = { prompt_position = "top" },
+    prompt_prefix = " ",
+    sorting_strategy = "ascending",
+    preview = {
+      filesize_limit = 5,
+      timeout = 150,
+      treesitter = true,
+      filesize_hook = function(filepath, bufnr, opts)
+        local path = Path:new(filepath)
+        local height = vim.api.nvim_win_get_height(opts.winid)
+        local lines = vim.split(path:head(height), "[\r]?\n")
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+      end,
+    },
+    dynamic_preview_title = true,
     mappings = {
       i = {
         ["<esc>"] = actions.close
@@ -9,3 +25,8 @@ tele.setup {
     },
   }
 }
+tele.load_extension("file_browser")
+tele.load_extension("fzf")
+tele.load_extension("frecency")
+tele.load_extension("emoji")
+tele.load_extension("projects")
