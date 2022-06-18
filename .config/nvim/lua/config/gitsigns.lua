2c4fa2baae3f7646d19819542dade48fc93ae14b
James Clark <james@jamesclark.dev>
2022-06-18 12:35:49 +0100

    Remove nvim

--- .config/nvim/lua/config/gitsigns.lua
+++
@@ -1,8 +0,0 @@
-require("gitsigns").setup({
-	signcolumn = true,
-	numhl = true,
-	current_line_blame = true,
-	current_line_blame_opts = {
-		virt_text_pos = "right_align",
-	},
-})
--- .config/nvim/lua/custom/octo.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-M.my_prs = function()
-	local me = require("octo.gh").get_user_name()
-	require("octo.picker").prs({ author = me })
-end
-
-return M
